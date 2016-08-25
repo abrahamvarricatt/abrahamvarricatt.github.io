85b2aa6669c904b01606b8dcb52176d424e37dcd
Abraham Varricatt <abraham.varricatt@goibibo.com>
2016-08-25 11:21:40 +0530

    bare-bones nikola setup

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# ref: https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+set -e # Exit with nonzero exit code if anything fails
+
+function doCompile {
+  nikola build
+}
+
+# Run compile script
+doCompile
+
