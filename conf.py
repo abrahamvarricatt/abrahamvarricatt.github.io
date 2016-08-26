8c3d343e41cbb1652739cfa26eb9ee7718b05022
Abraham Varricatt <abrahamvarricatt@users.noreply.github.com>
2016-08-27 00:57:34 +0530

    site url to HTTPS
    change default site URL to use HTTPS

--- conf.py
+++ conf.py
@@ -6,7 +6,7 @@ import time
 # Data about this site
 BLOG_AUTHOR = "chronodekar"
 BLOG_TITLE = "Note To Self"
-SITE_URL = "http://note2self.abraham-v.com/"
+SITE_URL = "https://note2self.abraham-v.com/"
 BLOG_EMAIL = "no@email.here"
 BLOG_DESCRIPTION = "Snippets of information"
 
